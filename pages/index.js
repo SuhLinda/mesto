4557b8be71b2c85d200bc298db887347bd23ab4d
Linda Suhovenko <suhovenko_linda@mail.ru>
2022-11-05 21:13:57 +0300

    feature/js

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+let editButton = document.querySelector('.profile__info_edit-button');
+let formPopUp = document.querySelector('.popup');
+let closeButton = formPopUp.querySelector('.popup__button-close');
+let saveButton = formPopUp.querySelector('.popup__button-save');
+
+function showPopUp() {
+  formPopUp.classList.add('.popup__opened');
+};
+
+function hidePopUp() {
+  formPopUp.classList.remove('.popup__opened');
+};
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__name_input');
+let jobInput = document.querySelector('.popup__description_input');
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    nameInput.textContent = nameInput.value;
+    jobInput.textContent = jobInput.value;
+    hidePopUp();
+}
+
+editButton.addEventListener('click', showPopUp());
+
+closeButton.addEventListener('click', hidePopUp());
+
+saveButton.addEventListener('submit', formSubmitHandler()); 
