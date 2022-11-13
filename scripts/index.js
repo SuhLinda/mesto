2f07d97d4a7176fd0f1cd8141df3c63eb34ce6e5
Linda Suhovenko <suhovenko_linda@mail.ru>
2022-11-14 00:04:53 +0300

    feature/elementcard

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,45 @@ let newName = document.querySelector('.profile__info-title');
 let jobInput = document.getElementById('description-input');
 let newJob = document.querySelector('.profile__info-subtitle');
 
+const elementsContainer = document.querySelector('.elements');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+]; 
+
+// функция добавления карточек
+initialCards.forEach(function (element) {
+  const templateElement = document.querySelector('#template__element').content;
+  const elementCard = templateElement.cloneNode(true);
+
+  elementCard.querySelector('.element__image').src = element.link;
+  elementCard.querySelector('.element__text').textContent = element.name;
+  elementsContainer.append(elementCard);
+})
+
 // функция открытия popUp
 function showPopUp() {
     nameInput.value = newName.textContent;
