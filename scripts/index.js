624f2200bcae78deadcb70b623856c478a9cb68e
Linda Suhovenko <suhovenko_linda@mail.ru>
2022-11-14 19:20:49 +0300

    feature/popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,22 @@
 // определение переменных
 let buttonEdit = document.querySelector('.profile__info-edit');
 let formPopUp = document.querySelector('.popup');
-let closeButton = formPopUp.querySelector('.popup__button-close');
+let buttonClose = formPopUp.querySelector('.popup__button-close');
 let formElement = document.getElementById('popup__form');
 let nameInput = document.getElementById('name-input');
 let newName = document.querySelector('.profile__info-title');
 let jobInput = document.getElementById('description-input');
 let newJob = document.querySelector('.profile__info-subtitle');
-
 const elementsContainer = document.querySelector('.elements');
+
+let buttonAdd = document.querySelector('.profile__add-button');
+let formPopUpAdd = document.getElementById('popup__card');
+let buttonCloseAdd = formPopUpAdd.querySelector('.popup__button-close_card');
+let formElementAdd = document.getElementById('popup__form__card');
+let nameInputCard = document.getElementById('name-input_card');
+let newNameCard = document.querySelector('.element__text');
+let linkInput = document.getElementById('image-input');
+let newlink = document.querySelector('.profile__info-subtitle');
 
 const initialCards = [
   {
@@ -67,7 +75,30 @@ function submitHandlerForm (evt) {
     hidePopUp()
 }
 
+// функция открытия popUpCard
+function showPopUpCard() {
+  nameInputCard.value = 'Название';
+  linkInput.value = 'Ссылка на картинку';
+  formPopUpAdd.classList.add('popup_opened');
+}
+
+//функция закрытия popUpCard
+function hidePopUpCard() {
+  formPopUpAdd.classList.remove('popup_opened');
+}
+
+// функция присвоения введённых данных card
+function submitAddForm (evt) {
+  evt.preventDefault();
+  newNameCard.textContent = nameInputCard.value;
+  newlink.src = linkInput.value;
+  hidePopUpCard()
+}
+
 // слушатели клика
 buttonEdit.addEventListener('click', showPopUp);
-closeButton.addEventListener('click', hidePopUp);
+buttonClose.addEventListener('click', hidePopUp);
 formElement.addEventListener('submit', submitHandlerForm);
+buttonAdd.addEventListener('click', showPopUpCard);
+buttonCloseAdd.addEventListener('click', hidePopUpCard);
+formElementAdd.addEventListener('click', submitAddForm);
