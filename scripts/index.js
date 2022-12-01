4c32673cf5a8a06a8f7545be6943dbcd0a9fd109
Linda Suhovenko <suhovenko_linda@mail.ru>
2022-12-01 23:11:46 +0300

    hotfix/js

--- scripts/index.js
+++ scripts/index.js
@@ -4,8 +4,11 @@ const popUpEdit = document.querySelector('.popup__edit');//окно popUp edit
 const buttonClose = document.querySelectorAll('.popup__button-close');// кнопка закрытия popUpS
 const buttonAdd = document.querySelector('.profile__add-button');// кнопка открытия окна добавления карточек popUpAdd
 const popUpAdd = document.querySelector('.popup__card');//окно popUp card
-
+const imgZoom = document.querySelector('.popup-zoom__image');
+const textZoom = document.querySelector('.popup-zoom__subtitle');
+const buttonZoom = document.querySelector('.popup-zoom__image');//клик для открытия popUpZoom
 const popUpZoom = document.getElementById('popup-zoom');//окно popUp zoom
+const elements = document.querySelector('.elements');
 
 //// закрытие PopUpS
 buttonClose.forEach(close => {
@@ -14,7 +17,21 @@ buttonClose.forEach(close => {
   })
 })
 
-const btnOpen = document.querySelectorAll('.open');
+// функция открытия popUpS
+function openPopUp(popUp) {
+  popUp.classList.add('popup_opened');
+  return popUp;
+}
+
+// функция присвоения введённых данных
+function submitEditForm (evt) {
+  evt.preventDefault();
+  newName.textContent = nameInput.value;
+  newJob.textContent = jobInput.value;
+  hidePopUpCard()//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+}
+  
+
 
 const formEditElement = document.getElementById('popup__form');
 const nameInput = document.getElementById('name-input');
@@ -25,92 +42,50 @@ const formAddElement = document.getElementById('popup__form_card');
 const nameInputCard = document.getElementById('name-input_card');
 const linkInput = document.getElementById('image-input');
 const elementsContainer = document.querySelector('.elements');
-const templateElement = document.getElementById('template__element').content;
-
-
-
-
-  // функция открытия popUpS
-function openPopUp(popUp) {
-  popUp.classList.add('popup_opened');
-  return popUp;
-}
-
-buttonEdit.addEventListener('click', openPopUp);
-
-
-  /*btnOpen.forEach(open => {
-    open.addEventListener('click', (evt) => {
-
-      /*if (open.classList.contains('.profile__info-edit')) {
-        popUpEdit.classList.add('popup_opened');
-      } else if (open.target.classList.contains('.profile__add-button')){
-        popUpAdd.classList.add('popup_opened');
-      } else if (open.target.classList.contains('.element__image')){
-        popUpZoom.classList.add('popup_opened');
-      }*/
-
-      /*const modalId = this.getAttribute('data-modal')
-      modalEl = document.querySelector('.modal[data-modal = "'+ modalId + '"]');
-      modalEl.classList.add('popup_opened');*/
-      //evt.preventDefault();
-
-      /*if (open.style.id = 'popup__edit') {
-        popUpEdit.classList.add('popup_opened');
-      } 
-        if (open.style.id = 'popup__card') {
-          popUpAdd.classList.add('popup_opened')
-        }*/
-      
-      /*open[0] = document.querySelector('.popup__edit').classList.add('popup_opened');
-      open[1] = document.querySelector('.popup__card').classList.add('popup_opened');
-      open[2] = popUpZoom.classList.add('popup_opened');*/
-    //})
-  //})
-
-
-
-
-
-/*function showPopUp() {
-  nameInput.value = newName.textContent;
-  jobInput.value = newJob.textContent;
-  popUpEdit.classList.add('popup_opened');
-  form.reset();
-}
-
-// функция открытия popUpCard
-function showPopUpCard() {
-
-  popUpAdd.classList.add('popup_opened');
-  form.reset();
-}*/
+const templateElement = document.getElementById('template__element');
 
 
 // функция создания карточек
-function createCard () {}
+function createCard () {
+  const elementCard = templateElement.content.cloneNode(true);
+  const img = elementCard.querySelector('.element__image');
+  const text = elementCard.querySelector('.element__text');
+  const buttonLike = elementCard.querySelector('.element__stroke');
+  const buttonDelete = elementCard.querySelectorAll('.element__delete');
+  img.src = link;
+  img.alt = name;
+  text.textContent = name;
+
+  img.addEventListener('click', () => {
+    openPopUp(popUpZoom);
+    imgZoom.src = img.src;
+    img.alt = text.textContent;
+    textZoom.textContent = text.textContent;
+  })
+  buttonLike.addEventListener('click', (evt) => evt.target.classList.toggle('element__stroke_active'));
+  buttonDelete.addEventListener('click', (evt) => evt.target.closest('.template__element').remove());
+
+  return elementCard;
+}
 
 
 
 // функция добавления карточек
-initialCards.forEach(function (element) {
-  const elementCard = templateElement.cloneNode(true);
-  elementCard.querySelector('.element__image').src = element.link;
-  elementCard.querySelector('.element__text').textContent = element.name;
+initialCards.forEach((card) => renderElement(elements, createCard(item.name, item.link)));
+
+
   elementCard.querySelector('.element__image').alt = element.name;
-  
-  
   // открытие popUpZoom
-  elementCard.querySelector('.element__image').addEventListener('click', function () {
+  lementCard.querySelector('.element__image').addEventListener('click', function () {
     popUpZoom.classList.add('popup_opened');
     const imgZoom = document.getElementById('popup-zoom__image').src = element.link;
     const textZoom = document.getElementById('popup-zoom__subtitle').textContent = element.name;
   })
   elementsContainer.append(elementCard);
-})
+
 
 // buttonLike
-const buttonLike = document.querySelectorAll('.element__stroke');
+/*const buttonLike = document.querySelectorAll('.element__stroke');
 buttonLike.forEach(like => {
   like.addEventListener('click', function (evt) {
     like.classList.toggle('element__stroke_active');
@@ -126,18 +101,12 @@ buttonDelete.forEach(del => {
   })
 })
 
-// функция присвоения введённых данных
-function submitHandlerForm (evt) {
-    evt.preventDefault();
-    newName.textContent = nameInput.value;
-    newJob.textContent = jobInput.value;
-    hidePopUpCard()//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-}
+
 
 // функция присвоения введённых данных card
 function submitAddForm (evt) {
   evt.preventDefault();
-  const elementCard = templateElement.cloneNode(true);
+  const elementCard = templateElement.content.cloneNode(true);
   elementCard.querySelector('.element__image').src = linkInput.value;
   elementCard.querySelector('.element__text').textContent = nameInputCard.value;
   elementsContainer.prepend(elementCard);
@@ -158,10 +127,11 @@ function submitAddForm (evt) {
     const imgZoom = document.getElementById('popup-zoom__image').src = linkInput.value;
     const textZoom = document.getElementById('popup-zoom__subtitle').textContent = nameInputCard.value;
   })*/
-}
+
 
 // слушатели клика
-
-formEditElement.addEventListener('submit', submitHandlerForm);
-//buttonAdd.addEventListener('click', showPopUpCard);
+buttonEdit.addEventListener('click', () => {openPopUp(popUpEdit), submitEditForm});//слушает клик и открывает popUpEdit
+formEditElement.addEventListener('submit', submitEditForm);//слушает клик и заносит данные в EditForm
+buttonAdd.addEventListener('click', () => {openPopUp(popUpAdd)});//слушает клик и открывает popUpAdd
 formAddElement.addEventListener('submit', submitAddForm);
+buttonZoom.addEventListener('click', () => {openPopUp(popUpZoom)});//слушает клик и открывает popUpZoom
